4377c152bf2425648ac32273ac176e4fb160cace
Matt Wong <mattwong59@gmail.com>
2019-01-26 19:41:33 -0800

    Add components

---
+++ src/components/form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                FORM 
+            </React.Fragment>
+        );
+    }
+};
+
+export default Form;
---
+++ src/components/titles.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Titles extends Component {
+    render() {
+        return  (
+            <h1>Titles</h1>
+        );
+    }
+};
+
+export default Titles 
