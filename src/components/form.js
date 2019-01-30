d08a833400a630482e703127d24bf0679f3092cb
Matt Wong <mattwong59@gmail.com>
2019-01-29 20:59:37 -0800

    Add styling to inputs

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = (props) => (
     <form onSubmit={props.getWeather}> 
-        <label htmlFor='city'>City</label>
+        <label htmlFor='city' autofocus>City</label>
         <input type="text" name="city" />
         <label htmlFor='country'>Country</label>
         <input type="text" name="country" />
