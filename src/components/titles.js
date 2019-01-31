e30492368f3fa9e22194ab70b8f4c4d753cf84d2
Matt Wong <mattwong59@gmail.com>
2019-01-30 22:41:32 -0800

    Position titles

--- src/components/titles.js
+++ src/components/titles.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Titles = () => (
-    <React.Fragment>
-        <h1>Weather</h1>
-        <h3>Enter a city and country to check out the weather</h3>
-    </React.Fragment>
+    <div className="titles">
+        <h1 className="main-title">Weather</h1>
+        <h3 className="subtitle">Enter a city and country to check out the weather</h3>
+    </div>
 )
 
 export default Titles 
